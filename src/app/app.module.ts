66c38567d285919e67968f0642bddab3c6b50426
VuLong <vulongpt@gmail.com>
2022-06-21 23:29:23 +0700

    initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderMenuComponent } from './shared/header-menu/header-menu.component';
+import { HeaderTopComponent } from './shared/header-top/header-top.component';
+import { LayoutComponent } from './shared/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderTopComponent,
+    HeaderMenuComponent,
+    BreadcrumbComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
