a1e3e2bcf2bb66c995327b3863586099dc4b49e9
VuLong <vulongpt@gmail.com>
2022-06-23 22:31:08 +0700

    add header top

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
@@ -8,6 +7,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderMenuComponent } from './shared/header-menu/header-menu.component';
 import { HeaderTopComponent } from './shared/header-top/header-top.component';
 import { LayoutComponent } from './shared/layout/layout.component';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { LayoutComponent } from './shared/layout/layout.component';
     BreadcrumbComponent,
     FooterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MenubarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/header-top/header-top.component.ts
+++ src/app/shared/header-top/header-top.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header-top',
   templateUrl: './header-top.component.html',
-  styleUrls: ['./header-top.component.scss']
+  styleUrls: ['./header-top.component.scss'],
 })
-export class HeaderTopComponent implements OnInit {
+export class HeaderTopComponent {
+  constructor() {}
 
-  constructor() { }
+  items: MenuItem[] = [];
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Language',
+        icon: 'pi pi-fw pi-file',
+        items: [
+          {
+            label: 'English',
+            icon: 'pi pi-fw pi-book',
+          },
+          {
+            label: 'Vietnamese',
+            icon: 'pi pi-fw pi-book',
+          },
+        ],
+      },
+      {
+        label: 'Currency',
+        icon: 'pi pi-fw pi-currency',
+        items: [
+          {
+            label: 'USD',
+            icon: 'pi pi-fw pi-align-left',
+          },
+          {
+            label: 'VND',
+            icon: 'pi pi-fw pi-align-right',
+          },
+        ],
+      },
+    ];
   }
-
 }
